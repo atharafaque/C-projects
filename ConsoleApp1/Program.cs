d486fa3ece043dfce118d813a2215c4f1456dfba
Athar Afaque <80690156+atharafaque@users.noreply.github.com>
2024-04-19 10:48:53 +0530

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+int age = -23;
+Console.WriteLine(age);
+Console.WriteLine(int.MaxValue);
+Console.WriteLine(int.MinValue);
+
+
+long bigNumber = -900000000L;
+Console.WriteLine(bigNumber);
+System.Console.WriteLine(long.MaxValue);
+System.Console.WriteLine(long.MinValue);
+
+Console.ReadLine();
