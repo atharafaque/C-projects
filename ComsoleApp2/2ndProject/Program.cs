1b68a8674c8ab1d7472e0600449dfee0858fb6b2
Athar Afaque <80690156+atharafaque@users.noreply.github.com>
2024-04-19 15:24:05 +0530

    first commit

---
+++ ComsoleApp2/2ndProject/Program.cs
@@ -0,0 +1,15 @@
+using System.Runtime.Intrinsics.Arm;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string name = "Aba";
+        char letter = 'a';
+        System.Console.WriteLine(name);
+        System.Console.WriteLine(letter);
+
+        
+        Console.ReadLine();
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,32 +1,46 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 
-int age = -23;
-Console.WriteLine(age);
-Console.WriteLine(int.MaxValue);
-Console.WriteLine(int.MinValue);
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int X;
+        int y;
+        int z;
 
 
-long bigNumber = -900000000L;
-Console.WriteLine(bigNumber);
-System.Console.WriteLine(long.MaxValue);
-System.Console.WriteLine(long.MinValue);
-
-double negative = -55.2D;
-System.Console.WriteLine(negative);
-System.Console.WriteLine(double.MaxValue);
-System.Console.WriteLine(double.MinValue);
 
 
-float precision  = 5.000001F;
-System.Console.WriteLine(precision);
-System.Console.WriteLine(float.MaxValue);
-System.Console.WriteLine(float.MinValue);
+
+        var age = -23;
+        Console.WriteLine(age);
+        Console.WriteLine(int.MaxValue);
+        Console.WriteLine(int.MinValue);
 
 
-decimal money = 14.99M;
-System.Console.WriteLine(money);
-System.Console.WriteLine(decimal.MaxValue);
-System.Console.WriteLine(decimal.MinValue);
+        long bigNumber = -900000000L;
+        Console.WriteLine(bigNumber);
+        Console.WriteLine(long.MaxValue);
+        Console.WriteLine(long.MinValue);
 
-Console.ReadLine();
+        double negative = -55.2D;
+        Console.WriteLine(negative);
+        Console.WriteLine(double.MaxValue);
+        Console.WriteLine(double.MinValue);
+
+
+        float precision = 5.000001F;
+        Console.WriteLine(precision);
+        Console.WriteLine(float.MaxValue);
+        Console.WriteLine(float.MinValue);
+
+
+        decimal money = 14.99M;
+        Console.WriteLine(money);
+        Console.WriteLine(decimal.MaxValue);
+        Console.WriteLine(decimal.MinValue);
+
+        Console.ReadLine();
+    }
+}
