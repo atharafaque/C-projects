4d659f15ad8f17bec26ccac81b4d2ed72d036c78
Athar Afaque <80690156+atharafaque@users.noreply.github.com>
2024-04-19 15:14:18 +0530

    first commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 int age = -23;
 Console.WriteLine(age);
@@ -10,5 +11,22 @@ long bigNumber = -900000000L;
 Console.WriteLine(bigNumber);
 System.Console.WriteLine(long.MaxValue);
 System.Console.WriteLine(long.MinValue);
+
+double negative = -55.2D;
+System.Console.WriteLine(negative);
+System.Console.WriteLine(double.MaxValue);
+System.Console.WriteLine(double.MinValue);
+
+
+float precision  = 5.000001F;
+System.Console.WriteLine(precision);
+System.Console.WriteLine(float.MaxValue);
+System.Console.WriteLine(float.MinValue);
+
+
+decimal money = 14.99M;
+System.Console.WriteLine(money);
+System.Console.WriteLine(decimal.MaxValue);
+System.Console.WriteLine(decimal.MinValue);
 
 Console.ReadLine();
